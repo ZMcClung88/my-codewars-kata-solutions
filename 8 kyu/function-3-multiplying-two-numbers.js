8f45e6010f30542e02b634fd9886590c36325bc9
Zac McClung <zac.mcclung370@gmail.com>
2017-10-23 22:35:50 -0600

    adding function #3 multiplying two numbers solution

---
+++ 8 kyu/function-3-multiplying-two-numbers.js
@@ -0,0 +1,4 @@
+// This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+
+// Write here your multiply-function
+const multiply = (a,b) => a * b;
