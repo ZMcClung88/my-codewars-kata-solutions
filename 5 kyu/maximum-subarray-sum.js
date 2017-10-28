712dd2f00c3a1fb90f0d73ed953eee594a5a2927
Zac McClung <zac.mcclung370@gmail.com>
2017-10-28 16:33:09 -0600

    adding best practice

--- 5 kyu/maximum-subarray-sum.js
+++ 5 kyu/maximum-subarray-sum.js
@@ -33,3 +33,14 @@ const maxSequence = arr => {
     }
     return max;
 };
+
+///////////////////   BEST PRACTICES  //////////////////////////
+ar maxSequence = function(arr){
+  var min = 0, ans = 0, i, sum = 0;
+  for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+  return ans;
+}
