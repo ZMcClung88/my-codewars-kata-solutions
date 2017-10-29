7078cb88f9378833874b8910e1047810af172324
Zac McClung <zac.mcclung370@gmail.com>
2017-10-28 22:18:42 -0600

    adding best pracitce

--- 7 kyu/nickname-generator.js
+++ 7 kyu/nickname-generator.js
@@ -33,3 +33,15 @@ const nicknameGenerator = name => {
     return name.substr(0,3);
   }
 }
+
+
+///////////////////   BEST PRACTICES  //////////////////////////
+function nicknameGenerator(name) {
+  if (name.length < 4) {
+    return "Error: Name too short";
+  } else if ("aeiou".indexOf(name[2].toLowerCase()) != -1) {
+    return name.slice(0, 4);
+  } else {
+    return name.slice(0, 3);
+  }
+}
