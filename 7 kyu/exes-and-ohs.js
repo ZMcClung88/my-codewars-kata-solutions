a64034819df0788811167f45a4c5590ca28f4384
Zac McClung <zac.mcclung370@gmail.com>
2017-11-02 21:20:35 -0600

    adding exes and ohs solution

---
+++ 7 kyu/exes-and-ohs.js
@@ -0,0 +1,35 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contains any char.
+//
+// Examples input/output:
+//
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // 
+
+const XO = str => {
+    //code here
+    let strArr = str.toLowerCase().split('');
+    let xArr = [];
+    let oArr = [];
+    let result = '';
+
+    strArr.map(letter => {
+      if(letter === 'x') {
+        xArr.push(letter);
+      } else if(letter === 'o') {
+        oArr.push(letter);
+      }
+    });
+
+
+    if(xArr.length === 0 && oArr.length === 0) {
+      result = true;
+    } else if(xArr.length === oArr.length) {
+      result = true;
+    } else {
+      result = false;
+    }
+
+    return result;
+}
