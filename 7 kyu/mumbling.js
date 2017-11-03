eb7ea4207f6239b566a20f68b2c24c97bcf287c7
Zac McClung <zac.mcclung370@gmail.com>
2017-11-02 22:01:43 -0600

    adding best practice

--- 7 kyu/mumbling.js
+++ 7 kyu/mumbling.js
@@ -15,3 +15,9 @@ const accum = (s) => {
   }
   return letters.join('-')
 }
+
+
+//////////////// BEST PRACTICE //////////////////////////
+function accum(s) {
+  return s.split('').map((x,index) => x.toUpperCase()+Array(index+1).join(x.toLowerCase())).join('-');
+}
