c6ea18d47415bae04fd8cf12a0871b9a95229161
Zac McClung <zac.mcclung370@gmail.com>
2017-11-26 21:52:12 -0700

    Adding best practice

--- 4 kyu/valid-braces.js
+++ 4 kyu/valid-braces.js
@@ -38,3 +38,24 @@ const validBraces = (braces) => {
   }
   return !bracesArr.length;
 };
+
+/////////// BEST PRACTICE //////////////
+function validBraces(braces){
+  var matches = { '(':')', '{':'}', '[':']' };
+  var stack = [];
+  var currentChar;
+
+  for (var i=0; i<braces.length; i++) {
+    currentChar = braces[i];
+
+    if (matches[currentChar]) { // opening braces
+      stack.push(currentChar);
+    } else { // closing braces
+      if (currentChar !== matches[stack.pop()]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0; // any unclosed braces left?
+}
