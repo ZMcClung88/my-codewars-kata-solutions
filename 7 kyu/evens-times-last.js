7bf8413d9cb52b89ed1114433c641bd81cc22a59
Zac McClung <zac.mcclung370@gmail.com>
2017-12-02 20:39:15 -0700

    adding best practice

--- 7 kyu/evens-times-last.js
+++ 7 kyu/evens-times-last.js
@@ -17,3 +17,10 @@ const evenLast = (numbers) => {
 
   return sum * last;
 }
+
+/////////// BEST PRACTICE //////////////
+function evenLast(numbers) {
+  var arr = numbers.filter(  (currentValue, index, array) => index % 2 === 0).reduce( (sum,val)=>sum+val,0);
+  var rst = (numbers.length > 0)?(arr*numbers[numbers.length-1]):0;
+  return rst;
+}
