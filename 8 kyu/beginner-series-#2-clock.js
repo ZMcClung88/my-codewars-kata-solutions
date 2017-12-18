f91c5d05bf44b2694ff3462f6d69faa2b245087b
Zac McClung <zac.mcclung370@gmail.com>
2017-12-17 21:30:26 -0700

    adding beginner series #2: clock javascript solution

---
+++ 8 kyu/beginner-series-#2-clock.js
@@ -0,0 +1,18 @@
+// Clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
+//
+// Your task is to make 'Past' function which returns time converted to miliseconds.
+//
+// #####Example:
+//
+// past(0, 1, 1) == 61000
+// Note! h, m and s will be only Natural numbers! Waiting for translations and Feedback! Thanks!
+//
+
+const past = (h, m, s) => {
+  //#Happy Coding! ^_^
+  let secMil = s * 1000;
+  let minMil = m * 60000;
+  let hourMil = h * 3600000;
+
+  return secMil + minMil + hourMil;
+}
