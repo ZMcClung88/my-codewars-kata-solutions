38987500ef6943bf9b2e5312fd50455445e89a11
Zac McClung <zac.mcclung370@gmail.com>
2018-01-21 21:16:33 -0700

    added best practice

--- 7 kyu/coding-3min-hacker-and-change.js
+++ 7 kyu/coding-3min-hacker-and-change.js
@@ -60,5 +60,10 @@ const change = (name,items,payment) => {
   } else {
     return Number(changeDue.toFixed(2));
   }
+}
 
+//////////// BEST PRACTICE \\\\\\\\\\\\
+function change(name, items, payment){
+  const res = items.reduce((x, y) => x - y, payment)
+  return +(name == 'John' ? res < 0 ? 0 : res * 2 - ~~(res) : res).toFixed(2)
 }
