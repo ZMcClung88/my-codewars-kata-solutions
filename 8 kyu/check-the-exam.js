5237931848cce0ff75d2bab73b19de780b188f9a
Zac McClung <zac.mcclung370@gmail.com>
2018-01-26 20:10:44 -0700

    adding check the exam javascript solution

---
+++ 8 kyu/check-the-exam.js
@@ -0,0 +1,28 @@
+// The first input array contains the correct answers to an exam, like ["a", "a", "b", "d"]. The second one is "answers" array and contains student's answers.
+//
+// The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer(empty string).
+//
+// If the score < 0, return 0.
+//
+// For example:
+//
+// checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+// checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+// checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+// checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+const checkExam = (array1, array2) => {
+ // good luck
+ let results = 0;
+
+ for(var i = 0; i < array1.length; i++) {
+   if(array1[i] === "" || array2[i] === "") {
+     results += 0;
+   } else if(array1[i] === array2[i]) {
+     results += 4;
+   } else {
+     results -= 1;
+   }
+ }
+ return results < 0 ? 0 : results;
+}
