9dddcf30f0c82d5c30bd25587e9a3cbe28828aa0
Zac McClung <zac.mcclung370@gmail.com>
2018-01-27 21:04:09 -0700

    added best practice

--- 8 kyu/fix-your-code-before-your-garden-dies.js
+++ 8 kyu/fix-your-code-before-your-garden-dies.js
@@ -10,3 +10,6 @@ const rainAmount = mm => {
          return "Your plant has had more than enough water for today!"
     };
 }
+
+//////////// BEST PRACTICE \\\\\\\\\\\\
+const rainAmount = mm => (mm<40) ? "You need to give your plant " + (40 - mm) + "mm of water" : "Your plant has had more than enough water for today!"
