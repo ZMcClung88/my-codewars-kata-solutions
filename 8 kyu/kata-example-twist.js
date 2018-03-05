5b378b6cab832883628171dae814ec87b4e6d369
Zac McClung <zac.mcclung370@gmail.com>
2018-03-04 21:39:36 -0700

    adding kata example twsit javascript solution

---
+++ 8 kyu/kata-example-twist.js
@@ -0,0 +1,8 @@
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// add the value "codewars" to the websites array 1,000 times
+var websites = [];
+
+for (var i = 0; i < 1000; i++) {
+  websites.push('codewars');
+}
