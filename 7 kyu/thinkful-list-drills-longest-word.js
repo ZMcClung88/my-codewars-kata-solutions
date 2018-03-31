0fb8b09dbe0ecef8c4b0907009c6714a4677056c
Zac McClung <zac.mcclung370@gmail.com>
2018-03-30 21:51:33 -0600

    adding thinkful list drills: Longest word javascript solution

---
+++ 7 kyu/thinkful-list-drills-longest-word.js
@@ -0,0 +1,18 @@
+// Write a function longest() that takes one argument, a list of words, and returns the length of the longest word in the list.
+//
+// For example:
+//
+// >>> words = ['simple', 'is', 'better', 'than', 'complex']
+// >>> longest(words)
+// 7
+// Do not modify the input list.
+
+const longest = words => {
+  let longLength = 0;
+
+  words.map(word => {
+    word.length > longLength ? (longLength = word.length) : null;
+  });
+
+  return longLength;
+};
