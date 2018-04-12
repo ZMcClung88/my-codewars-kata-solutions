2ade463b070f2e339544d2f986062d4889d54099
Zac McClung <zac.mcclung370@gmail.com>
2018-04-11 21:42:59 -0600

    adding stringy strings javascript solution

---
+++ 8 kyu/stringy-strings.js
@@ -0,0 +1,16 @@
+// write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+//
+// the string should start with a 1.
+//
+// a string with size 6 should return :'101010'.
+//
+// with size 4 should return : '1010'.
+//
+// with size 12 should return : '101010101010'.
+//
+// The size will always be positive and will only use whole numbers.
+
+const stringy = size => {
+  newStr = Array(Math.floor(size / 2) + 1).join('10');
+  return size % 2 === 0 ? newStr : newStr.concat(1);
+};
